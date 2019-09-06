1a823641ac5a3077ae91de3fec9cb89dc3cf6c72
marwinburesch <marwin@posteo.de>
2019-09-06 09:34:27 +0200

    add functions

--- modules/input.js
+++ modules/input.js
@@ -1,1 +1,31 @@
 // TODO: move entries here | add input function
+
+function get(sel, target = document) {
+  return target.querySelector(sel);
+}
+
+function getAll(sel, target = document) {
+  return Array.from(target.querySelectorAll(sel));
+}
+
+function updateTotal() {
+  const total = getFilteredEntries().reduce(
+    (prev, curr) => prev + curr.hours,
+    0
+  );
+  totalEl.innerHTML = total;
+}
+
+function resetForm() {
+  topicEl.value = "";
+  hoursEl.value = "";
+  topicEl.focus();
+}
+
+function renderSingleEntry(entry) {
+  const el = `<div class="entry">
+      <em>${entry.topic}</em>
+      <strong>${entry.hours}</strong>
+    </div>`;
+  entriesContainer.insertAdjacentHTML("beforeend", el);
+}
